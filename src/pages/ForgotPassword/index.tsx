ec24b709c9c572ab044d404eb452389bd9e38751
Lucas Barbosa Fonseca <lufonsecabarbosa@hotmail.com>
2021-02-01 12:38:56 -0300

    current version of the gobarber frontend

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,56 +1,59 @@
-import React, { useCallback, useRef } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import React, { useCallback, useRef, useState } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../service/api';
+
+import { useToast } from '../../context/toast';
+
+import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { useAuth } from '../../context/auth';
-import { useToast } from '../../context/toast';
-import getValidationErrors from '../../utils/getValidationErros';
-
 import { Container, Content, AnimationContainer, Background } from './style';
 
-interface SignInFormData {
+interface ForgotPasswordFormData {
   email: string;
-  password: string;
 }
 
-const SignIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
-
-  const { signIn } = useAuth();
   const { addToast } = useToast();
 
-  const history = useHistory();
-
-  const handleSubmit = useCallback(async (data: SignInFormData) => {
+  const handleSubmit = useCallback(async (data: ForgotPasswordFormData) => {
     try {
+      setLoading(true);
       formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string()
-          .required('Senha obrigatória'),
       });
 
       await schema.validate(data, {
         abortEarly: false,
       });
 
-      await signIn({
+      await api.post('/password/forgot', {
         email: data.email,
-        password: data.password,
       });
 
-      history.push('/dashboard');
+      //history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'E-mail de recuperação enviado',
+        description:
+          'Enviamos um e-mail para confirmar a recuperação de senha, cheque sua caixa de entrada',
+      });
     } catch (err) {
 
       if (err instanceof Yup.ValidationError) {
@@ -63,45 +66,39 @@ const SignIn: React.FC = () => {
 
       addToast({
         type: 'error',
-        title: 'Erro na autenticação',
-        description: 'Ocorreu um erro ao fazer login, cheque suas credenciais.',
+        title: 'Erro na recuperação de senha',
+        description: 'Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.',
       });
+
+    } finally {
+      setLoading(false);
     }
-  }, [signIn, addToast, history]);
+  }, [addToast]);
 
   return (
     <Container>
+      <Background />
+
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="Logo GoBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>
+            <h1>Recuperar senha</h1>
 
             <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
-
-            <Button type="submit">Entrar</Button>
-
-            <Link to="/forgot-password">Esqueci minha senha</Link>
+            <Button loading={loading} type="submit">Recuperar</Button>
           </Form>
 
-          <Link to="/signup">
-            <FiLogIn />
-            Criar conta
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
-
-      <Background />
     </Container>
   );
-}
+};
 
-export default SignIn;
+export default ForgotPassword;
